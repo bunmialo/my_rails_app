d714ad4767b0d689e48a8c2512aec62c58c89fed
bunmi alo <bunmialo@yahoo.com>
2017-06-07 19:21:14 -0400

    first commit

---
+++ my_rails_app/config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+  
+  resources :quotes
+
+  root 'welcome#index'
+end
+
+
+# Prefix Verb   URI Pattern                Controller#Action
+# welcome_index GET    /welcome/index(.:format)   welcome#index
+#        quotes GET    /quotes(.:format)          quotes#index
+#               POST   /quotes(.:format)          quotes#create
+#     new_quote GET    /quotes/new(.:format)      quotes#new
+#    edit_quote GET    /quotes/:id/edit(.:format) quotes#edit
+#         quote GET    /quotes/:id(.:format)      quotes#show
+#               PATCH  /quotes/:id(.:format)      quotes#update
+#               PUT    /quotes/:id(.:format)      quotes#update
+#               DELETE /quotes/:id(.:format)      quotes#destroy
+#          root GET    /                          welcome#index
