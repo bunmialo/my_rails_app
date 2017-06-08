a72b34e8bec4dbe73f362719d3c19396702fbc23
bunmi alo <bunmialo@yahoo.com>
2017-06-08 15:59:31 -0400

    got CR of CRUD done

--- my_rails_app/app/controllers/quotes_controller.rb
+++ my_rails_app/app/controllers/quotes_controller.rb
@@ -1,2 +1,28 @@
 class QuotesController < ApplicationController
+  def index
+    @quotes = Quote.all
+  end
+
+  def show
+    @quote = Quote.find(params[:id])
+  end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    @quote = Quote.new(quote_params)
+    
+    if @quote.save
+    redirect_to @quote
+  else
+    render 'new'
+    end
+  end
+
+  private
+  def quote_params
+    params.require(:quote).permit(:title, :text)
+  end
 end
---
+++ my_rails_app/app/models/quote.rb
@@ -0,0 +1,4 @@
+class Quote < ApplicationRecord
+  validates :title, presence: true,
+                    length: { minimum: 5 }
+end
